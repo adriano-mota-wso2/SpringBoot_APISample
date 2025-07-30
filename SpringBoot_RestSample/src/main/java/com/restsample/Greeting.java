0979b26c1d118101c6639a499996026e5e493b80
“Adriano <adriano@wso2.coml>
2025-07-30 07:34:10 -0300

    Initial Commit

---
+++ SpringBoot_RestSample/src/main/java/com/restsample/Greeting.java
@@ -0,0 +1,5 @@
+package com.restsample;
+
+public record Greeting(long id, String content) { }
+
+
---
+++ SpringBoot_RestSample/src/main/java/com/restsample/SpringBootRestSampleApplication.java
@@ -0,0 +1,13 @@
+package com.restsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestSampleApplication.class, args);
+	}
+
+}
